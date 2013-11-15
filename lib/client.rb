86a77383dacb0204ac6c737c34caaa86035ab841
bonzofenix <bonzofenix@gmail.com>
2013-11-15 18:54:46 -0300

    cleanup on client

--- lib/client.rb
+++ lib/client.rb
@@ -42,38 +42,26 @@ class Client
   end
 
   def self.clients
-    YAML.load_file("client.yml")
+    begin
+      YAML.load_file("client.yml")
+    rescue
+      warn '''Check that you have an client.env file in your project with
+      the following entry.
+      gitlab:
+        endpoint: http://gitlab.com/api/v3/
+      other_server:
+        endpoint: other_endpoint.com
+      '''
+      {}
+    end
   end
 
-  # begin
-    clients.each do |name, info|
-      Class.new(Base) do
-        self.endpoint= info.fetch('endpoint')
-      end.tap do |client_class|
-        const_set(name.camelize, client_class)
-      end
+  self.clients.each do |name, info|
+    Class.new(Base) do
+      self.endpoint= info.fetch('endpoint')
+    end.tap do |client_class|
+      const_set(name.camelize, client_class)
     end
-  # rescue
-    # puts $@
-    # warn '''Check that you have an client.env file in your project with
-    # the following entry.
-    # gitlab:
-      # endpoint: http://gitlab.com/api/v3/
-    # other_server:
-      # endpoint: other_endpoint.com
-      # '''
-  # end
-
-
-  def self.gitlab(method, resource, params ={}, opts = nil)
-    uri = URI("https://gitlab.com/api/v3/#{resource}")
-    res = if method == :get
-            uri.query = URI.encode_www_form(params)
-            Net::HTTP.get_response(uri)
-          else
-            Net::HTTP.post_form(uri, params)
-          end
-    res
   end
 end
 
