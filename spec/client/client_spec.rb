85c12db8aee8be98b0fdffbfd6524ebb81b3b1c8
bonzofenix <bonzofenix@gmail.com>
2013-11-15 18:42:11 -0300

    tests

---
+++ spec/client/client_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Client do
+  let(:client){ described_class }
+
+
+  describe 'when there is a config' do
+
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'bundle/setup'
+require 'client'
+
+
+Rspec.configure do |config|
+end
+
