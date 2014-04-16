6cd77554071a94abcec1f600fb5950087bbd4cf4
Alan Moran <bonzofenix@gmail.com>
2014-04-16 09:11:40 -0400

    checking rack env in travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 require 'webmock/rspec'
 
 Dir.chdir File.expand_path('fixtures', File.dirname(__FILE__))
+puts 'HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',ENV['RACK_ENV']
 require 'client'
 
 
